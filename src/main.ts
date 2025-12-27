700667faf866a901fd70bb2e8e7637f93e1fc861
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2025-12-26 22:50:13 -0500

    add command that open's current daily note

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,18 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin } from "obsidian";
+import {
+	App,
+	Editor,
+	MarkdownView,
+	Modal,
+	Notice,
+	Plugin,
+	moment,
+} from "obsidian";
 import {
 	DEFAULT_SETTINGS,
 	MyPluginSettings,
 	SampleSettingTab,
 } from "./settings";
+import { getAllDailyNotes, getDailyNote } from "obsidian-daily-notes-interface";
 
 // Remember to rename these classes and interfaces!
 
@@ -15,6 +24,15 @@ export default class UnhealthyBedtimePlugin extends Plugin {
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
 		this.addSettingTab(new SampleSettingTab(this.app, this));
+
+		// Register commands
+		this.addCommand({
+			id: "open-todays-daily-note-in-current-leaf",
+			name: "Open today's daily note while considering configured bedtime",
+			callback: () => {
+				this.openTodaysDailyNote();
+			},
+		});
 	}
 
 	onunload() {}
@@ -22,12 +40,19 @@ export default class UnhealthyBedtimePlugin extends Plugin {
 	async loadSettings() {
 		this.settings = {
 			...DEFAULT_SETTINGS,
-			...(await this.loadData()) as Partial<MyPluginSettings>,
+			...((await this.loadData()) as Partial<MyPluginSettings>),
 		};
 	}
 
 	async saveSettings() {
 		await this.saveData(this.settings);
+	}
+
+	async openTodaysDailyNote() {
+		const allDailyNotes = getAllDailyNotes();
+		const todaysDailyNote = getDailyNote(moment(), allDailyNotes);
+		const leaf = this.app.workspace.getLeaf()
+		leaf.openFile(todaysDailyNote)
 	}
 }
 
